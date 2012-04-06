73562c77bee05ee32f32e9a4e98f45ed116740fe
Samuel Cochran <sj26@sj26.com>
2012-04-06 13:37:13 +0800

    [#4] Allow sending multiple MAIL FROM: commands per RFC2821

--- lib/mail_catcher/smtp.rb
+++ lib/mail_catcher/smtp.rb
@@ -1,6 +1,17 @@
 require 'eventmachine'
 
 class MailCatcher::Smtp < EventMachine::Protocols::SmtpServer
+  # We override EM's mail from processing to allow multiple mail-from commands
+  # per [RFC 2821](http://tools.ietf.org/html/rfc2821#section-4.1.1.2)
+  def process_mail_from sender
+    if @state.include? :mail_from
+      @state -= [:mail_from, :rcpt, :data]
+      receive_reset
+    end
+
+    super
+  end
+
   def current_message
     @current_message ||= {}
   end
