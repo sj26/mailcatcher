709a86015ddca9472437c3e1ee518e17ea71b666
Jason Nochlin <hundredwatt@gmail.com>
2012-09-09 12:44:10 -0400

    Add a 'Deliver' action to the UI for a message
    "Deliver" button is replaced with text while waiting for delivery

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -3,18 +3,23 @@
     __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
 
   jQuery.expr[':'].icontains = function(a, i, m) {
-    var _ref, _ref2;
-    return ((_ref = (_ref2 = a.textContent) != null ? _ref2 : a.innerText) != null ? _ref : "").toUpperCase().indexOf(m[3].toUpperCase()) >= 0;
+    var _ref, _ref1;
+    return ((_ref = (_ref1 = a.textContent) != null ? _ref1 : a.innerText) != null ? _ref : "").toUpperCase().indexOf(m[3].toUpperCase()) >= 0;
   };
 
   MailCatcher = (function() {
 
     function MailCatcher() {
       this.nextTab = __bind(this.nextTab, this);
+
       this.previousTab = __bind(this.previousTab, this);
+
       this.openTab = __bind(this.openTab, this);
+
       this.selectedTab = __bind(this.selectedTab, this);
+
       this.getTab = __bind(this.getTab, this);
+
       var _this = this;
       $('#messages tr').live('click', function(e) {
         e.preventDefault();
@@ -89,13 +94,17 @@
       key('up', function() {
         var id;
         id = _this.selectedMessage() || 1;
-        if (id > 1) id -= 1;
+        if (id > 1) {
+          id -= 1;
+        }
         return _this.loadMessage(id);
       });
       key('down', function() {
         var id;
         id = _this.selectedMessage() || _this.messagesCount();
-        if (id < _this.messagesCount()) id += 1;
+        if (id < _this.messagesCount()) {
+          id += 1;
+        }
         return _this.loadMessage(id);
       });
       key('⌘+up, ctrl+up', function() {
@@ -131,7 +140,9 @@
     };
 
     MailCatcher.prototype.formatDate = function(date) {
-      if (typeof date === "string") date && (date = this.parseDate(date));
+      if (typeof date === "string") {
+        date && (date = this.parseDate(date));
+      }
       date && (date = this.offsetTimeZone(date));
       return date && (date = date.toString("dddd, d MMM yyyy h:mm:ss tt"));
     };
@@ -159,7 +170,9 @@
     MailCatcher.prototype.previousTab = function(tab) {
       var i;
       i = tab || tab === 0 ? tab : this.selectedTab() - 1;
-      if (i < 0) i = this.tabs().length - 1;
+      if (i < 0) {
+        i = this.tabs().length - 1;
+      }
       if (this.getTab(i).is(":visible")) {
         return i;
       } else {
@@ -170,7 +183,9 @@
     MailCatcher.prototype.nextTab = function(tab) {
       var i;
       i = tab ? tab : this.selectedTab() + 1;
-      if (i > this.tabs().length - 1) i = 0;
+      if (i > this.tabs().length - 1) {
+        i = 0;
+      }
       if (this.getTab(i).is(":visible")) {
         return i;
       } else {
@@ -179,7 +194,9 @@
     };
 
     MailCatcher.prototype.haveMessage = function(message) {
-      if (message.id != null) message = message.id;
+      if (message.id != null) {
+        message = message.id;
+      }
       return $("#messages tbody tr[data-message-id=\"" + message + "\"]").length > 0;
     };
 
@@ -214,7 +231,9 @@
 
     MailCatcher.prototype.loadMessage = function(id) {
       var _this = this;
-      if ((id != null ? id.id : void 0) != null) id = id.id;
+      if ((id != null ? id.id : void 0) != null) {
+        id = id.id;
+      }
       id || (id = $('#messages tr.selected').attr('data-message-id'));
       if (id != null) {
         $('#messages tbody tr:not([data-message-id="' + id + '"])').removeClass('selected');
@@ -249,6 +268,26 @@
           } else {
             $('#message .metadata .attachments').hide();
           }
+          $('#message .views .deliver a').click(function(e) {
+            var $deliver, deliver_html,
+              _this = this;
+            e.preventDefault();
+            $deliver = $(this).parent();
+            deliver_html = $(this).parent().html();
+            $deliver.text('Delivering...');
+            return $.ajax({
+              url: "/messages/" + id + "/deliver",
+              type: 'POST',
+              success: function() {
+                $deliver.html(deliver_html);
+                return alert('Message successfully delivered');
+              },
+              error: function() {
+                $deliver.html(deliver_html);
+                return alert('An error occurred while attempting to deliver this message');
+              }
+            });
+          });
           $('#message .views .download a').attr('href', "/messages/" + id + ".eml");
           if ($('#message .views .tab.analysis.selected').length) {
             return _this.loadMessageAnalysis();
@@ -289,7 +328,9 @@
       var _this = this;
       return $.getJSON('/messages', function(messages) {
         return $.each(messages, function(i, message) {
-          if (!_this.haveMessage(message)) return _this.addMessage(message);
+          if (!_this.haveMessage(message)) {
+            return _this.addMessage(message);
+          }
         });
       });
     };
