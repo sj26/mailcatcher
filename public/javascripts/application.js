b7e16ae794bace88d1a29b64eca229a4a165f9c3
Ivan Kuchin <tadump+git@gmail.com>
2013-08-21 23:45:48 +0200

    rebuilt js and css

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,6 +1,6 @@
 (function() {
   var MailCatcher,
-    _this = this;
+    __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
 
   jQuery.expr[':'].icontains = function(a, i, m) {
     var _ref, _ref1;
@@ -8,24 +8,13 @@
   };
 
   MailCatcher = (function() {
-
     function MailCatcher() {
+      this.nextTab = __bind(this.nextTab, this);
+      this.previousTab = __bind(this.previousTab, this);
+      this.openTab = __bind(this.openTab, this);
+      this.selectedTab = __bind(this.selectedTab, this);
+      this.getTab = __bind(this.getTab, this);
       var _this = this;
-      this.nextTab = function(tab) {
-        return MailCatcher.prototype.nextTab.apply(_this, arguments);
-      };
-      this.previousTab = function(tab) {
-        return MailCatcher.prototype.previousTab.apply(_this, arguments);
-      };
-      this.openTab = function(i) {
-        return MailCatcher.prototype.openTab.apply(_this, arguments);
-      };
-      this.selectedTab = function() {
-        return MailCatcher.prototype.selectedTab.apply(_this, arguments);
-      };
-      this.getTab = function(i) {
-        return MailCatcher.prototype.getTab.apply(_this, arguments);
-      };
       $('#messages tr').live('click', function(e) {
         e.preventDefault();
         return _this.loadMessage($(e.currentTarget).attr('data-message-id'));
