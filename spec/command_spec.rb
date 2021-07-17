58670fc0eeca8e4daca8925ca844814f88c4ef5b
Samuel Cochran <sj26@sj26.com>
2021-07-17 12:10:13 +1000

    Spec help output works

--- spec/command_spec.rb
+++ spec/command_spec.rb
@@ -8,4 +8,12 @@ RSpec.describe "mailcatcher command" do
         .to_stdout_from_any_process
     end
   end
+
+  context "--help" do
+    it "shows help then exits" do
+      expect { system %(mailcatcher --help) }
+        .to output(a_string_including("MailCatcher v#{MailCatcher::VERSION}") & a_string_including("--help") & a_string_including("Display this help information"))
+        .to_stdout_from_any_process
+    end
+  end
 end
