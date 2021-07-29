6815f296fda555e5263720e41469c4637a872220
Ahmed Gagan <ahmedgagan6@gmail.com>
2021-07-29 11:49:48 +0530

    Update spec/smtp_protocol_spec.rb
    Co-authored-by: Samuel Cochran <sj26@sj26.com>

--- spec/smtp_protocol_spec.rb
+++ spec/smtp_protocol_spec.rb
@@ -5,15 +5,15 @@ require 'async/io/socket'
 require 'mail_catcher'
 require 'net/smtp'
 
-message = <<~END_OF_MESSAGE
-    From: Your Name <your@mail.address>
-    To: Destination Address <someone@example.com>
-    Subject: test message
-    Date: Sat, 23 Jun 2001 16:26:43 +0900
-    Message-Id: <unique.message.id.string@example.com>
-  #{'  '}
-    This is a test message.
-END_OF_MESSAGE
+message = <<~MESSAGE
+  From: Your Name <your@mail.address>
+  To: Destination Address <someone@example.com>
+  Subject: test message
+  Date: Sat, 23 Jun 2001 16:26:43 +0900
+  Message-Id: <unique.message.id.string@example.com>
+
+  This is a test message.
+MESSAGE
 
 SMTP_PORT = 20_025
 HTTP_PORT = 20_080
