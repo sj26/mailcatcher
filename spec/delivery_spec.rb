2c25f6e211bff5dbe3e5324ac91cfa97f2a8b53f
Samuel Cochran <sj26@sj26.com>
2022-03-05 10:56:20 +1100

    Spec attachments

--- spec/delivery_spec.rb
+++ spec/delivery_spec.rb
@@ -39,12 +39,26 @@ RSpec.describe MailCatcher, type: :feature do
     page.find("#message header .format.source a")
   end
 
+  def attachment_header_element
+    page.find("#message header .metadata dt.attachments")
+  end
+
+  def attachment_contents_element
+    page.find("#message header .metadata dd.attachments")
+  end
+
+  def first_attachment_element
+    attachment_contents_element.find("ul li:first-of-type a")
+  end
+
   def body_element
     page.find("body")
   end
 
   it "catches and displays a plain text message as plain text and source" do
     deliver_example("plainmail")
+
+    # Do not reload, make sure that the message appears via websockets
 
     expect(page).to have_selector("#messages table tbody tr:first-of-type", text: "Plain mail")
 
@@ -76,6 +90,8 @@ RSpec.describe MailCatcher, type: :feature do
 
   it "catches and displays an html message as html and source" do
     deliver_example("htmlmail")
+
+    # Do not reload, make sure that the message appears via websockets
 
     expect(page).to have_selector("#messages table tbody tr:first-of-type", text: "Test HTML Mail")
 
@@ -109,6 +125,8 @@ RSpec.describe MailCatcher, type: :feature do
 
   it "catches and displays a multipart message as text, html and source" do
     deliver_example("multipartmail")
+
+    # Do not reload, make sure that the message appears via websockets
 
     expect(page).to have_selector("#messages table tbody tr:first-of-type", text: "Test Multipart Mail")
 
@@ -151,6 +169,8 @@ RSpec.describe MailCatcher, type: :feature do
   it "catches and displays a multipart UTF8 message as text, html and source" do
     deliver_example("multipartmail-with-utf8")
 
+    # Do not reload, make sure that the message appears via websockets
+
     expect(page).to have_selector("#messages table tbody tr:first-of-type", text: "Test Multipart UTF8 Mail")
 
     expect(message_from_element).to have_text(DEFAULT_FROM)
@@ -192,23 +212,64 @@ RSpec.describe MailCatcher, type: :feature do
   it "catches and displays an unknown message as source" do
     deliver_example("unknownmail")
 
+    # Do not reload, make sure that the message appears via websockets
+
     skip
   end
 
   it "catches and displays a message with multipart attachments" do
     deliver_example("attachmail")
 
-    skip
+    # Do not reload, make sure that the message appears via websockets
+
+    expect(page).to have_selector("#messages table tbody tr:first-of-type", text: "Test Attachment Mail")
+
+    expect(message_from_element).to have_text(DEFAULT_FROM)
+    expect(message_to_element).to have_text(DEFAULT_TO)
+    expect(message_subject_element).to have_text("Test Attachment Mail")
+    expect(Time.parse(message_received_element.text)).to be <= Time.now + 5
+
+    message_row_element.click
+
+    expect(source_tab_element).to be_visible
+    expect(plain_tab_element).to be_visible
+    expect(attachment_header_element).to be_visible
+
+    plain_tab_element.click
+
+    within_frame do
+      expect(page).to have_text "This is plain text"
+    end
+
+    expect(first_attachment_element).to be_visible
+    expect(first_attachment_element).to have_text("attachment")
+
+    # Downloading via the browser is hard, so just grab from the URI directly
+    expect(Net::HTTP.get(URI.join(Capybara.app_host, first_attachment_element[:href]))).to eql("Hello, I am an attachment!\r\n")
+
+    source_tab_element.click
+
+    within_frame do
+      expect(page).to have_text "Content-Type: multipart/mixed"
+      expect(page).to have_text "This is plain text"
+
+      expect(page).to have_text "Content-Disposition: attachment"
+      # Too hard to add expectations on the transfer encoded attachment contents
+    end
   end
 
   it "doesn't choke on messages containing dots" do
     deliver_example("dotmail")
+
+    # Do not reload, make sure that the message appears via websockets
 
     skip
   end
 
   it "doesn't choke on messages containing quoted printables" do
     deliver_example("quoted_printable_htmlmail")
+
+    # Do not reload, make sure that the message appears via websockets
 
     skip
   end
