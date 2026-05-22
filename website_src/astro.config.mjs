a73281b649d98ee59c035851f091d96ac134fcdf
spaquet <176050+spaquet@users.noreply.github.com>
2026-05-22 09:27:38 -0700

    Updating to Taiwind 4.3 (#29)
    Updating the website to Astro 6.3 and Tailwind 4.3

--- website_src/astro.config.mjs
+++ website_src/astro.config.mjs
@@ -1,15 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import tailwindcss from "@tailwindcss/vite";
 import icon from "astro-icon";
+
+
+import tailwindcss from '@tailwindcss/vite';
+
 
 // https://astro.build/config
 export default defineConfig({
   // security: { csp: true }, // cannot be activated when in development
   base: '/mailcatcher/',
+
+  integrations: [icon()],
+
   vite: {
     plugins: [tailwindcss()],
   },
-
-  integrations: [icon()],
 });
